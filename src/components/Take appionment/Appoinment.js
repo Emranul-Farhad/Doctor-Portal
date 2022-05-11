10f2657d1b68aa77d69c8af77dfb91564229b44f
unknown <emranulfarhad@gmail.com>
2022-05-11 23:18:15 +0600

    apoint part

---
+++ src/components/Take appionment/Appoinment.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import appointment from '../../Images/images/appointment.png'
+import doctor from '../../Images/images/doctor.png'
+
+
+
+const Appoinment = () => {
+    return (
+        <div className='mt-20'>
+            <section style={{
+                background: `url(${appointment})`
+            }}
+                className='flex justify-center items-center'>
+                <div className='flex-1 hidden md:block lg:block'>
+                    <img className='mt-[-130px]' src={doctor} alt="" />
+                </div>
+                <div className='flex-1 p-5'>
+                    <h3 className='text-2xl text-primary font-bold text-left mb-2'>Appointment</h3>
+                    <h2 className='mb-2 text-3xl text-white text-left '>Make an Appointment Today</h2>
+                    <p className='text-white text-left '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
+                    <button class=" mt-4 mx-5 btn btn-primary bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-bold">Take Appoinment </button>
+                </div>
+            </section>
+            {/* <Swiper
+					spaceBetween={30}
+					slidesPerView="auto"
+					centeredslide="false"
+					navigation
+					autoplay={true}
+					key={reviews.length}
+				>
+					{reviews.map((reviews, index) => (
+						<SwiperSlide key={index}>
+							<Testimonial reviews={reviews} />
+						</SwiperSlide>
+					))}
+				</Swiper> */}
+
+        </div>
+    );
+};
+
+export default Appoinment;
+
+
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import DoctorSaid from '../../components/Doctoe said/DoctorSaid';
 import Explain from '../../components/Explainations/Explain';
 import InfoCards from '../../components/Home page Information/InfoCards';
 import Serviceprovides from '../../components/Services we provides/Serviceprovides';
+import Appoinment from '../../components/Take appionment/Appoinment';
 import Homebanner from '../Home banner/Homebanner';
 
 
@@ -28,7 +29,11 @@ const Home = () => {
         <div className='mt-[131px]'> 
             <Serviceprovides></Serviceprovides>
         </div>
-
+{/* Take appoin ments */}
+        <div className='mt-[131px]'>
+            <Appoinment></Appoinment>
+        </div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam ullam maxime, repudiandae ea officia quas quibusdam quis, inventore consectetur esse temporibus, ipsam fugit quo possimus! Qui inventore perspiciatis doloribus praesentium repellat voluptate dolorem, magni libero vel at sapiente excepturi omnis rerum tempora rem consequuntur accusantium esse sint minima aliquam soluta?</p>
         </div>
     );
 };
